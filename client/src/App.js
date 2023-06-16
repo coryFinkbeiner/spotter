c4fee15b7e8d8f7198754e7d495e761edf2e21d8
steeper <portland.steeper@gmail.com>
2023-06-16 16:29:58 -0400

    LOGIN IS WORKING!!!

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+
+
+import SpotifyWebApi from "spotify-web-api-node"
+
 import './App.css';
 
+import useAuth from "./hooks/useAuth";
+
+const spotifyApi = new SpotifyWebApi({
+  clientId: "b5fd7277f6654b3e881be98a94afd5fc",
+})
+
+// const AUTH_URL =
+//   "https://accounts.spotify.com/authorize?client_id=b5fd7277f6654b3e881be98a94afd5fc&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state"
+
+
 function App() {
+
+  const AUTH_URL =
+  "https://accounts.spotify.com/authorize?client_id=b5fd7277f6654b3e881be98a94afd5fc&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state"
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Hello
+      <button onClick={() => (window.location.href = AUTH_URL)}>
+        Login With Spotify
+      </button>
     </div>
   );
 }
---
+++ client/src/hooks/useAuth.js
@@ -0,0 +1,47 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const useAuth = (code) => {
+  const [accessToken, setAccessToken] = useState();
+  const [refreshToken, setRefreshToken] = useState();
+  const [expiresIn, setExpiresIn] = useState();
+
+  useEffect(() => {
+    axios
+      .post("/login", {
+        code,
+      })
+      .then((res) => {
+        setAccessToken(res.data.accessToken);
+        setRefreshToken(res.data.refreshToken);
+        setExpiresIn(res.data.expiresIn);
+        window.history.pushState({}, null, "/");
+      })
+      .catch(() => {
+        window.location = "/";
+      });
+  }, [code]);
+
+  useEffect(() => {
+    if (!refreshToken || !expiresIn) return;
+    const interval = setInterval(() => {
+      axios
+        .post("/refresh", {
+          refreshToken,
+        })
+        .then((res) => {
+          setAccessToken(res.data.accessToken);
+          setExpiresIn(res.data.expiresIn);
+        })
+        .catch(() => {
+          window.location = "/";
+        });
+    }, (expiresIn - 60) * 1000);
+
+    return () => clearInterval(interval);
+  }, [refreshToken, expiresIn]);
+
+  return accessToken;
+};
+
+export default useAuth;
