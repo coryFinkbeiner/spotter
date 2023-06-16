eeb6c576ead53492eabc663cb710efb9977f9bfd
steeper <portland.steeper@gmail.com>
2023-06-16 17:06:09 -0400

    working on Dashboard fetch test

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,10 @@
 import Login from './components/Login'
 import Dashboard from "./components/Dashboard"
-
-import SpotifyWebApi from "spotify-web-api-node"
-
 import './App.css';
-
-import useAuth from "./hooks/useAuth";
-
-const spotifyApi = new SpotifyWebApi({
-  clientId: "b5fd7277f6654b3e881be98a94afd5fc",
-})
-
-
+import SpotifyWebApi from "spotify-web-api-node"
 
 function App() {
   const code = new URLSearchParams(window.location.search).get("code")
-
 
   return code ? <Dashboard code={code} /> : <Login />
 }
--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,6 +1,15 @@
 import React from 'react'
+import useAuth from "../hooks/useAuth";
+import SpotifyWebApi from "spotify-web-api-node"
+import axios from "axios"
 
-function Dashboard() {
+const spotifyApi = new SpotifyWebApi({
+  clientId: "b5fd7277f6654b3e881be98a94afd5fc",
+})
+
+function Dashboard({code}) {
+
+
   return (
     <div>Dashboard</div>
   )
