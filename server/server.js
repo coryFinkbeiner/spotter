c0dd6e0576d8b571cd10ec56cb7d04b49a8788b9
steeper <portland.steeper@gmail.com>
2023-06-16 19:40:23 -0400

    i think i solved the endpoint issue by adding full 3001 url

---
+++ server/server.js
@@ -0,0 +1,75 @@
+require("dotenv").config()
+const express = require("express")
+const cors = require("cors")
+const bodyParser = require("body-parser")
+const SpotifyWebApi = require("spotify-web-api-node")
+
+const path = require('path');
+
+const app = express()
+app.use(cors())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+
+app.post("/refresh", (req, res) => {
+  const refreshToken = req.body.refreshToken
+  const spotifyApi = new SpotifyWebApi({
+    redirectUri: process.env.REDIRECT_URI,
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    refreshToken,
+  })
+
+  spotifyApi
+    .refreshAccessToken()
+    .then(data => {
+      res.json({
+        accessToken: data.body.accessToken,
+        expiresIn: data.body.expiresIn,
+      })
+    })
+    .catch(err => {
+      console.log(err)
+      res.sendStatus(400)
+    })
+})
+
+app.post("/login", (req, res) => {
+  console.log('login HIT')
+  const code = req.body.code
+  const spotifyApi = new SpotifyWebApi({
+    redirectUri: process.env.REDIRECT_URI,
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+  })
+
+  spotifyApi
+    .authorizationCodeGrant(code)
+    .then(data => {
+      console.log('login data', data.body)
+      res.json({
+        accessToken: data.body.access_token,
+        refreshToken: data.body.refresh_token,
+        expiresIn: data.body.expires_in,
+      })
+    })
+    .catch(err => {
+      console.log('login', err)
+      res.sendStatus(400)
+    })
+})
+
+// Serve static files from the 'build' directory
+// app.use(express.static(path.resolve(__dirname, '..', 'client', 'build')));
+
+// Catch all route for serving the client application
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '..', 'client', 'public', 'index.html'));
+});
+
+// Your other routes and server configuration
+
+app.listen(3001, () => {
+  console.log('Server is running on port 3001');
+});
