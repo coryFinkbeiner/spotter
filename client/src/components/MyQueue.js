f9bc431f9d8773fd659d286e7e401c03fb7bf4ec
steeper <portland.steeper@gmail.com>
2023-06-28 19:53:19 -0400

    add to queue is working with parameters in axios POST

--- client/src/components/MyQueue.js
+++ client/src/components/MyQueue.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect }  from 'react'
 import useAuth from "../hooks/useAuth";
 import SpotifyWebApi from "spotify-web-api-node"
-// import { useDataContext } from '../hooks/useDataContext'
+import { useDataContext } from '../hooks/useDataContext'
 
 import axios from 'axios'
 
@@ -11,8 +11,8 @@ const spotifyApi = new SpotifyWebApi({
 
 function MyQueue() {
   const accessToken = useAuth()
-  // const { myQueue } = useDataContext()
-  // if (myQueue) console.log('0', myQueue[0])
+  const { myQueue } = useDataContext()
+  console.log('0', myQueue)
 
   useEffect(() => {
     if (!accessToken) return
@@ -21,13 +21,17 @@ function MyQueue() {
 
   useEffect(() => {
 
-    console.log({accessToken})
 
-    const url = 'https://api.spotify.com/v1/me/player/queue?uri=spotify%3Atrack%3A4iV5W9uYEdYUVa79Axb7Rh'
+
+    const url = 'https://api.spotify.com/v1/me/player/queue'
+    const uri = 'spotify:track:4iV5W9uYEdYUVa79Axb7Rh';
 
     axios.post(url, null, {
       headers: {
         Authorization: `Bearer ${accessToken}`
+      },
+      params: {
+        uri: uri
       }
     })
       .then(response => {
@@ -38,25 +42,6 @@ function MyQueue() {
       });
 
   }, [accessToken]);
-
-
-  // useEffect(() => {
-  //   if (myQueue) {
-  //     const uri = myQueue[0]
-  //     console.log('inside add to queue... uri', uri)
-
-  //     spotifyApi.setAccessToken(accessToken)
-
-
-  //     spotifyApi.addToQueue(uri)
-  //       .then(res => {
-  //         console.log('queue add SUCCESS')
-  //       })
-  //       .catch(err => console.log('add to queue error:', err))
-  //   } else {
-  //     console.log('my queue is empty')
-  //   }
-  // }, [accessToken, myQueue]);
 
   return (
     <div>MyQueue</div>
