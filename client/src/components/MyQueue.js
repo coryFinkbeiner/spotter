7e174d18ae412d8b0ead7eb578d48bc2caf03d50
steeper <portland.steeper@gmail.com>
2023-07-16 19:28:55 -0400

    rough draft of sidebar operational. i will add Search nav functionality now.

--- client/src/components/MyQueue.js
+++ client/src/components/MyQueue.js
@@ -38,6 +38,18 @@ function MyQueue() {
     dispatch({ type: 'SET_ACCESS_TOKEN', payload: accessToken })
   }, [accessToken])
 
+    useEffect(() => {
+      if (poppedTrack) {
+        // console.log('uri', poppedTrack.uri);
+        queuePOST(poppedTrack.uri, accessToken);
+
+        setTimeout(() => {
+          dispatch({ type: 'POP_QUEUE' })
+        }, poppedTrack.duration_ms - 3000);
+
+      }
+    }, [poppedTrack]);
+
   return (
     <div className='my-queue'>----------------MyQueue--------------------
       <button
@@ -45,7 +57,7 @@ function MyQueue() {
           dispatch({ type: 'POP_QUEUE' })
         }}
       >
-        Add To Spotify Queue
+        Start Spotify Playback Loop
       </button>
       {<ul>
         {myQueue?.map((track, index) => (
@@ -60,74 +72,13 @@ function MyQueue() {
 export default MyQueue
 
 
+
+// what if MyQueue expanded across Console when you open up advanced options? like the multiple queues idea.
+
+
+
+
+
 // am i going to be compounding the 3 seconds delay?
   // do i need to set the delay at first, of 3 seconds, but then after that, set the delay to the exact duration of the track?
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import React, { useState, useEffect }  from 'react'
-// import useAuth from "../hooks/useAuth";
-// import SpotifyWebApi from "spotify-web-api-node"
-// import { useDataContext } from '../hooks/useDataContext'
-// import axios from 'axios'
-
-// const spotifyApi = new SpotifyWebApi({
-//   clientId: "b5fd7277f6654b3e881be98a94afd5fc",
-// })
-
-// async function addToQueue(url, accessToken, uri) {
-//   try {
-//     const response = await axios.post(
-//       url,
-//       null,
-//       {
-//         headers: {
-//           Authorization: `Bearer ${accessToken}`,
-//         },
-//         params: {
-//           uri: uri,
-//         },
-//       }
-//     );
-//   } catch (error) {
-//     console.log('Error:', error);
-//   }
-// }
-
-// function MyQueue() {
-//   const accessToken = useAuth()
-//   const { myQueue, dispatch } = useDataContext()
-
-//   useEffect(() => {
-//     if (!accessToken) return
-//     spotifyApi.setAccessToken(accessToken)
-//     dispatch({ type: 'SET_ACCESS_TOKEN', payload: accessToken })
-//   }, [accessToken])
-
-//   return (
-//     <div>----------------MyQueue--------------------
-//       {<ul>
-//         {myQueue?.map((track, index) => (
-//           <li key={index}>{track.name} - {track.artists[0].name}</li>
-//         ))}
-//       </ul>}
-//     <div>-------------------------------------------</div>
-//     </div>
-//   )
-// }
-
-// export default MyQueue
