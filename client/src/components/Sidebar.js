a323b8d429196792348802f278db049ef866a2a0
steeper <portland.steeper@gmail.com>
2023-07-16 00:15:12 -0400

    refactoring radios, styling

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -3,24 +3,24 @@ import axios from 'axios';
 import { useDataContext } from '../hooks/useDataContext';
 
 
-function Tracks({ results, dispatch }) {
-  if (!results) {
-    return <div>Tracks not found</div>;
-  }
+// function Tracks({ results, dispatch }) {
+//   if (!results) {
+//     return <div>Tracks not found</div>;
+//   }
 
-  return (
-    <ul>
-      {results?.items?.map((item) => (
-        <li
-          key={item.track.id}
-          onClick={() => dispatch({ type: 'ADD_TO_QUEUE', payload: item.track })}
-        >
-          <div>{item.track.name}</div>
-        </li>
-      ))}
-    </ul>
-  );
-}
+//   return (
+//     <ul>
+//       {results?.items?.map((item) => (
+//         <li
+//           key={item.track.id}
+//           onClick={() => dispatch({ type: 'ADD_TO_QUEUE', payload: item.track })}
+//         >
+//           <div>{item.track.name}</div>
+//         </li>
+//       ))}
+//     </ul>
+//   );
+// }
 
 function Albums({ results, dispatch }) {
 
@@ -76,6 +76,92 @@ function Playlists({ results, dispatch }) {
 }
 
 
+// function Sidebar() {
+//   const [radio, setRadio] = useState('albums');
+//   const [results, setResults] = useState({});
+
+//   const { accessToken, dispatch } = useDataContext();
+
+//   useEffect(() => {
+//     const fetchData = async () => {
+//       try {
+//         const response = await axios.get(`https://api.spotify.com/v1/me/${radio}`, {
+//           headers: {
+//             Authorization: `Bearer ${accessToken}`,
+//           },
+//         });
+//         setResults(response.data);
+//       } catch (error) {
+//         console.log('Search Error:', error);
+//       }
+//     };
+
+//     fetchData();
+//   }, [radio, accessToken]);
+
+//   const handleRadioChange = (event) => {
+//     setRadio(event.target.value);
+//   };
+
+//   return (
+//     <div className='sidebar'>
+//       <div className='sidebar-1'>
+//         <div className='s-1-nav'>
+//           Home
+//         </div>
+//         <div className='s-1-nav'>
+//           Search
+//         </div>
+
+//       </div>
+
+//       <div className='sidebar-2'>
+//         <div className='s-2-1'>
+//           Your Library
+//         </div>
+//         <label>
+//           <input
+//             type="radio"
+//             value="albums"
+//             checked={radio === 'albums'}
+//             onChange={handleRadioChange}
+//           />
+//           Albums
+//         </label>
+//         <label>
+//           <input
+//             type="radio"
+//             value="playlists"
+//             checked={radio === 'playlists'}
+//             onChange={handleRadioChange}
+//           />
+//           Playlists
+//         </label>
+//       </div>
+
+//       {radio === 'albums' && (
+//         <Albums results={results} dispatch={dispatch} />
+//       )}
+
+//       {radio === 'playlists' && (
+//         <Playlists results={results} dispatch={dispatch} />
+//       )}
+//     </div>
+//   );
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
 function Sidebar() {
   const [radio, setRadio] = useState('albums');
   const [results, setResults] = useState({});
@@ -99,10 +185,6 @@ function Sidebar() {
     fetchData();
   }, [radio, accessToken]);
 
-  const handleRadioChange = (event) => {
-    setRadio(event.target.value);
-  };
-
   return (
     <div className='sidebar'>
       <div className='sidebar-1'>
@@ -119,24 +201,21 @@ function Sidebar() {
         <div className='s-2-1'>
           Your Library
         </div>
-        <label>
-          <input
-            type="radio"
-            value="albums"
-            checked={radio === 'albums'}
-            onChange={handleRadioChange}
-          />
-          Albums
-        </label>
-        <label>
-          <input
-            type="radio"
-            value="playlists"
-            checked={radio === 'playlists'}
-            onChange={handleRadioChange}
-          />
-          Playlists
-        </label>
+
+
+        <div className='s-2-2'>
+          <div
+            onClick={() => setRadio('albums')}
+          >
+            Albums
+          </div>
+          <div
+            onClick={() => setRadio('playlists')}
+          >
+            Playlists
+          </div>
+        </div>
+
       </div>
 
       {radio === 'albums' && (
@@ -150,11 +229,33 @@ function Sidebar() {
   );
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 export default Sidebar;
 
 // add icons
 // thicken font
+
 // refactor Albums and Playlists radios
+  // i think it's pretty simple, just add an onClick to the div
+    //
 
 
 
