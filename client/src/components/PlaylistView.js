b0247e9b6ccdddf0b8877d943a5ee48eb2c7178a
steeper <portland.steeper@gmail.com>
2023-07-16 22:07:15 -0400

    formatting Artist View / top view

--- client/src/components/PlaylistView.js
+++ client/src/components/PlaylistView.js
@@ -8,15 +8,32 @@ function PlaylistView() {
   return (
     <div className='AlbumPlaylistView'>
       <div className='view-top'>
-      <img
-        src={inView.images[0]?.url}
-        style={{
-          margin: '12px'
-        }}
-      />
+        <img className='view-top-img'
+          src={inView.images[0]?.url}
+
+        />
+        <div className='view-top-info'>
+          <div>
+            Playlist
+          </div>
+          <div>
+            0AC
+          </div>
+          <div
+            style={{
+
+              marginLeft: '3px',
+              padding: '2px'
+            }}
+          >
+            Bodhi - 16 songs, 1hr 10min
+          </div>
+        </div>
       </div>
     </div>
   )
 }
 
 export default PlaylistView
+
+// black background for now, instead of fancy gradient
