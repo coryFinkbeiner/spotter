62b04ed7925d94ef9f4bf728b9cb89b1c5b4a999
steeper <portland.steeper@gmail.com>
2023-08-02 16:50:13 -0400

    trying to add pg db by following chat gpt, and light reference to ssome sources i've gathered

--- server/server.js
+++ server/server.js
@@ -37,8 +37,6 @@ app.post("/refresh", (req, res) => {
 
 app.post("/login", (req, res) => {
 
-  console.log('hit')
-
   const code = req.body.code
   const spotifyApi = new SpotifyWebApi({
     redirectUri: process.env.REDIRECT_URI,
