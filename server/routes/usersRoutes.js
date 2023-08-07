1a0242d77436c04e175858fc49dc6adda870977f
steeper <portland.steeper@gmail.com>
2023-08-06 21:33:19 -0400

    fixed small error with .env redirect URI

--- server/routes/usersRoutes.js
+++ server/routes/usersRoutes.js
@@ -2,12 +2,8 @@ const express = require('express');
 const router = express.Router();
 const usersController = require('../controllers/usersController');
 
-// Route to create a new user
 router.post('/users', usersController.createUser);
 
-// Route to get a user by ID
 router.get('/users/:id', usersController.getUserById);
-
-// Add more routes for user operations if needed
 
 module.exports = router;
