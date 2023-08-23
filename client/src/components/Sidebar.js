a17fbe4d9a6d5e4a34a31cd070d014c20211edc7
steeper <portland.steeper@gmail.com>
2023-08-23 15:07:34 -0400

    may have made small changes, but key change was deleting results from dependencies in sidebar somewhere

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -32,51 +32,53 @@ function Playlists({ results, dispatch }) {
     return <div>Playlists not found</div>;
   }
 
-  return (
-    <div className='sidebar-render'>
-      {results?.items?.map((playlist) => (
-        <div className='sidebar-item-container'
-          key={playlist.id}
-          onClick={() => {
-            dispatch({ type: 'VIEW_IN_CONSOLE', payload: playlist })
-            dispatch({ type:'CHANGE_VIEW_TYPE', payload: 'PlaylistView' })
-          }}
-        >
-          <img
-            src={playlist.images[0]?.url}
-            style={{
-              borderRadius: '5px',
-              margin: '2px'
-            }}
-          />
-          <div
-            style={{
-              color: 'white',
-              marginLeft: '3px',
-              padding: '2px'
-            }}
-          >
-            <div
-              style={{
-                height: '50%',
-                fontWeight: 'bold',
-              }}
-            >
-              {playlist.name}
+  return <div>{results.href}</div>
 
-            </div>
-            <div
-              style={{
-                height: '50%'
-              }}
-            >
-              {playlist.owner.display_name}
-            </div>
-          </div>
-        </div>
-      ))}
-    </div>
-  )
+  // return (
+  //   <div className='sidebar-render'>
+  //     {results?.items?.map((playlist) => (
+  //       <div className='sidebar-item-container'
+  //         key={playlist.id}
+  //         onClick={() => {
+  //           dispatch({ type: 'VIEW_IN_CONSOLE', payload: playlist })
+  //           dispatch({ type:'CHANGE_VIEW_TYPE', payload: 'PlaylistView' })
+  //         }}
+  //       >
+  //         <img
+  //           src={playlist.images[0]?.url}
+  //           style={{
+  //             borderRadius: '5px',
+  //             margin: '2px'
+  //           }}
+  //         />
+  //         <div
+  //           style={{
+  //             color: 'white',
+  //             marginLeft: '3px',
+  //             padding: '2px'
+  //           }}
+  //         >
+  //           <div
+  //             style={{
+  //               height: '50%',
+  //               fontWeight: 'bold',
+  //             }}
+  //           >
+  //             {playlist.name}
+
+  //           </div>
+  //           <div
+  //             style={{
+  //               height: '50%'
+  //             }}
+  //           >
+  //             {playlist.owner.display_name}
+  //           </div>
+  //         </div>
+  //       </div>
+  //     ))}
+  //   </div>
+  // )
 }
 
 function Sidebar() {
@@ -94,6 +96,7 @@ function Sidebar() {
           },
         });
         setResults(response.data);
+        console.log(response.data)
       } catch (error) {
         console.log('Search Error:', error);
       }
