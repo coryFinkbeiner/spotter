2e90a1dd416c4fcd2b71d33e8ec3c127d13b3378
steeper <portland.steeper@gmail.com>
2023-08-24 14:25:43 -0400

    about to move Playlist and Album GETs to App.js

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -2,8 +2,6 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useDataContext } from '../hooks/useDataContext';
 
-import { Container, Row, Col } from 'react-bootstrap';
-
 
 function Albums({ results, dispatch }) {
 
@@ -32,53 +30,53 @@ function Playlists({ results, dispatch }) {
     return <div>Playlists not found</div>;
   }
 
-  return <div>{results.href}</div>
+  // return <div>{results.href}</div>
 
-  // return (
-  //   <div className='sidebar-render'>
-  //     {results?.items?.map((playlist) => (
-  //       <div className='sidebar-item-container'
-  //         key={playlist.id}
-  //         onClick={() => {
-  //           dispatch({ type: 'VIEW_IN_CONSOLE', payload: playlist })
-  //           dispatch({ type:'CHANGE_VIEW_TYPE', payload: 'PlaylistView' })
-  //         }}
-  //       >
-  //         <img
-  //           src={playlist.images[0]?.url}
-  //           style={{
-  //             borderRadius: '5px',
-  //             margin: '2px'
-  //           }}
-  //         />
-  //         <div
-  //           style={{
-  //             color: 'white',
-  //             marginLeft: '3px',
-  //             padding: '2px'
-  //           }}
-  //         >
-  //           <div
-  //             style={{
-  //               height: '50%',
-  //               fontWeight: 'bold',
-  //             }}
-  //           >
-  //             {playlist.name}
+  return (
+    <div className='sidebar-render'>
+      {results?.items?.map((playlist) => (
+        <div className='sidebar-item-container'
+          key={playlist.id}
+          onClick={() => {
+            dispatch({ type: 'VIEW_IN_CONSOLE', payload: playlist })
+            dispatch({ type:'CHANGE_VIEW_TYPE', payload: 'PlaylistView' })
+          }}
+        >
+          <img
+            src={playlist.images[0]?.url}
+            style={{
+              borderRadius: '5px',
+              margin: '2px'
+            }}
+          />
+          <div
+            style={{
+              color: 'white',
+              marginLeft: '3px',
+              padding: '2px'
+            }}
+          >
+            <div
+              style={{
+                height: '50%',
+                fontWeight: 'bold',
+              }}
+            >
+              {playlist.name}
 
-  //           </div>
-  //           <div
-  //             style={{
-  //               height: '50%'
-  //             }}
-  //           >
-  //             {playlist.owner.display_name}
-  //           </div>
-  //         </div>
-  //       </div>
-  //     ))}
-  //   </div>
-  // )
+            </div>
+            <div
+              style={{
+                height: '50%'
+              }}
+            >
+              {playlist.owner.display_name}
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  )
 }
 
 function Sidebar() {
