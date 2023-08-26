cd6a8854dcfb3a875f22d2d3819ff3732433725b
steeper <portland.steeper@gmail.com>
2023-08-26 00:34:33 -0400

    AlbumView is kinda first draft

--- client/src/components/AlbumView.js
+++ client/src/components/AlbumView.js
@@ -6,6 +6,8 @@ function AlbumView() {
   const { inView } = useDataContext()
 
   return (
+
+
     <div className='AlbumPlaylistView'>
       <div className='view-top'>
         <img className='view-top-img'
@@ -23,6 +25,98 @@ function AlbumView() {
           </div> */}
         </div>
     </div>
+
+
+    <div className="song-list"
+      style={{
+        overflowY: 'auto',
+          maxHeight: '460px'
+      }}
+    >
+      {inView.album.tracks.items.map((track, index) => (
+        <div
+          key={index}
+          style={{
+            display: 'flex',
+            alignItems: 'center',
+            padding: '8px',
+          }}
+        >
+            <div
+              style={{
+                padding: '15px',
+                color: 'grey',
+              }}
+            >
+              {index + 1}
+            </div>
+            <img
+              src={inView.album.images[2]?.url}
+              style={{
+                height: '38px',
+                borderRadius: '3px',
+                margin: '2px',
+              }}
+          />
+            <div
+              style={{
+                width: '225px'
+              }}
+            >
+              <div
+                style={{
+                  color: 'white',
+                  paddingRight: '15px',
+                  paddingLeft: '15px',
+                  whiteSpace: 'nowrap',
+                  overflow: 'hidden',
+                  textOverflow: 'ellipsis',
+
+                }}
+              >
+                {track.name}
+              </div>
+              <div
+                style={{
+                  color: 'grey',
+                  paddingRight: '15px',
+                  paddingLeft: '15px',
+                  whiteSpace: 'nowrap',
+                  overflow: 'hidden',
+                  textOverflow: 'ellipsis',
+                  fontSize: '13px'
+                }}
+              >
+                {track.artists[0].name}
+              </div>
+            </div>
+            <div
+                style={{
+                  color: 'grey',
+                  paddingRight: '15px',
+                  paddingLeft: '15px',
+                  whiteSpace: 'nowrap',
+                  overflow: 'hidden',
+                  textOverflow: 'ellipsis',
+                  fontSize: '13px',
+                  width: '160px'
+
+                }}
+              >
+                {inView.album.name}
+              </div>
+
+            <div
+              style={{
+                color: 'grey',
+                fontSize: '13px',
+              }}
+            >
+              3:30
+            </div>
+          </div>
+        ))}
+      </div>
 
 
 
