3c8221af65bc913912bca35e07158ef1ac080306
steeper <portland.steeper@gmail.com>
2023-08-28 23:39:21 -0400

    lining up top left of Search, coloring

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -4,6 +4,10 @@ import { useDataContext } from '../hooks/useDataContext';
 import Track from './Track'
 import Album from './Album'
 
+import { SearchIcon } from '@heroicons/react/outline'
+
+
+
 function Tracks({ results }) {
   if (!results.tracks) {
     return <div>No Results</div>
@@ -117,18 +121,17 @@ function Search() {
 
     <div
       style={{
-        backgroundColor: 'lightblue',
+        backgroundColor: 'rgb(18, 18, 18)',
         height: '100%',
         padding: '4px, 4px, 4px, 4px',
-        marginTop: '13px'
+        marginTop: '10px',
+        borderRadius: '8px',
       }}
     >
 
       {/* top container */}
       <div
         style={{
-          // backgroundColor: 'rgb(18, 18, 18)'
-          backgroundColor: 'white',
           margin: '6px',
           padding: '10px, 10px, 10px, 10px',
           height: '105px',
@@ -158,11 +161,16 @@ function Search() {
           >
             <div
               style={{
-                width: '42px'
+                width: '42px',
+                cursor: 'pointer'
               }}
-
+              onClick={() => setCount(count + 1)}
             >
-              M
+              <SearchIcon
+                style={{
+                  color: 'white'
+                }}
+              />
             </div>
             <input
               type="text"
@@ -177,27 +185,23 @@ function Search() {
               style={{
                 width: '42px'
               }}
-              onClick={() => setCount(count + 1)}
+
             >
-              S
+              X
             </div>
           </div>
 
           {/* Radio buttons container */}
           <div
             style={{
-              // backgroundColor: 'red',
               height: '100%',
               width: '100%',
               margin: '0px 0px 0px 0px',
               padding: '7px 4px 4px 4px',
               display: 'flex',
               flex: 'row'
-
-
             }}
           >
-
             <div
               style={{
                 backgroundColor: 'grey',
@@ -211,6 +215,7 @@ function Search() {
                 textAlign: 'center',
                 fontSize: '14px',
                 alignItems: 'center',
+                cursor: 'pointer',
               }}
               onClick={() => setRadio('artist')}
             >
@@ -230,6 +235,7 @@ function Search() {
                 textAlign: 'center',
                 fontSize: '14px',
                 alignItems: 'center',
+                cursor: 'pointer',
               }}
               onClick={() => setRadio('track')}
             >
@@ -249,6 +255,7 @@ function Search() {
                 textAlign: 'center',
                 fontSize: '14px',
                 alignItems: 'center',
+                cursor: 'pointer',
               }}
               onClick={() => setRadio('album')}
             >
