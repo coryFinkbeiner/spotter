b32151ef5e151da7c8e27b3774b32fe999ec4a04
steeper <portland.steeper@gmail.com>
2023-08-30 15:56:43 -0400

    Artist shaping up, img not rendering right tho

--- client/src/components/Album.js
+++ client/src/components/Album.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import { useDataContext } from '../hooks/useDataContext';
 import axios from 'axios'
 
--- client/src/components/Artist.js
+++ client/src/components/Artist.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useDataContext } from '../hooks/useDataContext';
 
 function Artist({
   index,
@@ -7,8 +8,61 @@ function Artist({
   item,
   id
 }) {
+  const [isHovering, setIsHovering] = useState(false);
+
   return (
-    <div>{id}</div>
+    <div
+      style={{
+        backgroundColor: isHovering ? 'rgb(44, 44, 44)' : 'rgb(28, 28, 28)',
+        height: '174px',
+        width: '127px',
+        margin: '0px 0px -18px 0px',
+        borderRadius: '5px',
+        padding: '8px 8px 8px 8px',
+        whiteSpace: 'nowrap',
+        overflow: 'hidden',
+        textOverflow: 'ellipsis',
+        cursor: 'pointer',
+      }}
+      onMouseEnter={() => setIsHovering(true)}
+      onMouseLeave={() => setIsHovering(false)}
+    >
+      <img
+        src={imageURL}
+        style={{
+          borderRadius: '5px',
+          width: '100%',
+          paddingBottom: '9px',
+
+        }}
+      />
+        <div
+          style={{
+            color: 'white',
+            fontWeight: 'bold',
+            fontSize: '13px',
+            paddingBottom: '3px'
+          }}
+        >
+          {artistName}
+        </div>
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'row',
+            color: 'lightgrey',
+            fontSize: '10px',
+          }}
+        >
+          <div
+            style={{
+
+            }}
+          >
+            Artist
+        </div>
+      </div>
+    </div>
   )
 }
 
