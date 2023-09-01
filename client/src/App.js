09c8d3bc28da541573d3a5eef43b0307a223405f
steeper <portland.steeper@gmail.com>
2023-09-01 17:26:04 -0400

    player is successfully stored in context again

--- client/src/App.js
+++ client/src/App.js
@@ -63,51 +63,51 @@ function App() {
     fetchPlaylists();
     fetchAlbums();
 
-    // const script = document.createElement("script");
-    // script.src = "https://sdk.scdn.co/spotify-player.js";
-    // script.async = true;
+    const script = document.createElement("script");
+    script.src = "https://sdk.scdn.co/spotify-player.js";
+    script.async = true;
 
-    // document.body.appendChild(script);
+    document.body.appendChild(script);
 
-    // window.onSpotifyWebPlaybackSDKReady = () => {
+    window.onSpotifyWebPlaybackSDKReady = () => {
 
-    //   const player = new window.Spotify.Player({
-    //       name: 'Web Playback SDK',
-    //       getOAuthToken: cb => { cb(accessToken); },
-    //       volume: 0.5
-    //   });
+      const player = new window.Spotify.Player({
+          name: 'Web Playback SDK',
+          getOAuthToken: cb => { cb(accessToken); },
+          volume: 0.5
+      });
 
-    //     // Ready
-    //   player.addListener('ready', ({ device_id }) => {
-    //     console.log('Ready with Device ID', device_id);
-    //   });
+        // Ready
+      player.addListener('ready', ({ device_id }) => {
+        console.log('Ready with Device ID', device_id);
+      });
 
-    //   // Not Ready
-    //   player.addListener('not_ready', ({ device_id }) => {
-    //     console.log('Device ID has gone offline', device_id);
-    //   });
+      // Not Ready
+      player.addListener('not_ready', ({ device_id }) => {
+        console.log('Device ID has gone offline', device_id);
+      });
 
-    //   player.addListener('initialization_error', ({ message }) => {
-    //     console.error(message);
-    //   });
+      player.addListener('initialization_error', ({ message }) => {
+        console.error(message);
+      });
 
-    //   player.addListener('authentication_error', ({ message }) => {
-    //       console.error(message);
-    //   });
+      player.addListener('authentication_error', ({ message }) => {
+          console.error(message);
+      });
 
-    //   player.addListener('account_error', ({ message }) => {
-    //       console.error(message);
-    //   });
+      player.addListener('account_error', ({ message }) => {
+          console.error(message);
+      });
 
-      // player.connect().then(success => {
-      //   if (success) {
-      //     console.log('The Web Playback SDK successfully connected to Spotify!');
-      //   }
-      // })
+      player.connect().then(success => {
+        if (success) {
+          console.log('The Web Playback SDK successfully connected to Spotify!');
+        }
+      })
 
-    //   dispatch({ type: 'SET_PLAYER', payload: player });
+      dispatch({ type: 'SET_PLAYER', payload: player });
 
-    // }
+    }
 
   }, [accessToken])
 
