e8cf5d866250d17617261b4eb36dd09e9751c2d3
steeper <portland.steeper@gmail.com>
2023-09-13 12:29:23 -0400

    Login incorporates AppLogin, Register and Login basic ternary set up

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,13 +1,21 @@
 import React, { useState } from 'react'
-import AppLogin from './AppLogin'
+// import AppLogin from './AppLogin'
+
+import { useDataContext } from '../hooks/useDataContext';
+
+
 
 const AUTH_URL =
   "https://accounts.spotify.com/authorize?client_id=b5fd7277f6654b3e881be98a94afd5fc&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state%20app-remote-control"
 
 function Login() {
-
   const [ isLoggedIn, setIsLoggedIn ] = useState(false)
 
+
+  const { dispatch } = useDataContext()
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [isNewUser, setIsNewUser] = useState(true);
 
 
   return (
@@ -16,7 +24,24 @@ function Login() {
         Login With Spotify
       </button>
     ) : (
-      <AppLogin setIsLoggedIn={setIsLoggedIn}/>
+      <div>
+        { isNewUser ? (
+
+          <div>
+            <h1>Register</h1>
+          </div>
+
+
+        ) : (
+
+          <div>
+            <h1>Login</h1>
+          </div>
+
+        )}
+
+      </div>
+
     )
   );
 }
