12b4a0137983d54e852746657b1d703af5d60227
steeper <portland.steeper@gmail.com>
2023-10-15 07:19:36 -0400

    FakeApp works

--- client/src/components/FakeApp.js
+++ client/src/components/FakeApp.js
@@ -4,15 +4,15 @@ const LeftHandComponent = ({ name }) => {
   return <h1 style={{ backgroundColor: 'green' }}>{name}</h1>;
 }
 
-const RightHandComponent = ({ name }) => {
+const RightHandComponent = ({ message }) => {
   return <p style={{ backgroundColor: 'red' }}>{message}</p>;
 }
 
 function FakeApp() {
   return (
     <SplitScreen leftWeight={1} rightWeight={3}>
-      <LeftHandComponent name='Shaun' />
-      <RightHandComponent message='Hello' />
+      <LeftHandComponent name="Shaun" />
+      <RightHandComponent message="Hello" />
     </SplitScreen>
   );
 }
--- client/src/index.js
+++ client/src/index.js
@@ -4,11 +4,24 @@ import './index.css';
 import App from './App';
 import { DataProvider } from './DataContext';
 
+
+import FakeApp from './components/FakeApp';
+
+
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <DataProvider>
-      <App />
+
+      {/* <App /> */}
+
+
+      <FakeApp />
+
+
+
     </DataProvider>
   </React.StrictMode>
 );
